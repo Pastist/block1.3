b0fb797a346ff3658221787ee3b35570ba90f19d
Sergey Prohorenko <93403270+Pastist@users.noreply.github.com>
2025-08-29 21:15:45 +0300

    1.5.320

--- js/main.js
+++ js/main.js
@@ -1,21 +1,20 @@
-const swiper = new swiper('.swiper', {
-  // Optional parameters
-  direction: 'horisontal',
-  loop: true,
-
-  // If we need pagination
+const swiper = new Swiper('.swiper', {
+  slidesPerView: "auto",
+  //spaceBetween: 0,
   pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  //navigation: {
-  //  nextEl: '.swiper-button-next',
-  //  prevEl: '.swiper-button-prev',
-  //},
-
-  // And if we need scrollbar
-  //scrollbar: {
-  //  el: '.swiper-scrollbar',
+  el: ".swiper-pagination",
+  clickable: true,
   },
 });
+const myDiv = document.getElementById('show-more_ikon');
+const toggleButton = document.getElementById('show_icon-readmore');
+
+  toggleButton.addEventListener('click', function() {
+      if (myDiv.style.display === 'none') {
+        myDiv.style.display = 'block';
+        toggleButton.textContent = 'Скрыть';
+      } else {
+        myDiv.style.display = 'none';
+        toggleButton.textContent = 'Показать';
+      }
+    });
