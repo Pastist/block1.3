ecc85700efb8f72622899c5f3e0dbb0514108cbf
Sergey Prohorenko <93403270+Pastist@users.noreply.github.com>
2025-08-31 22:52:42 +0300

    1.5.320

--- js/main.js
+++ js/main.js
@@ -1,20 +1,44 @@
 const swiper = new Swiper('.swiper', {
-  slidesPerView: "auto",
-  //spaceBetween: 0,
+  slidesPerView: 'auto',
+  spaceBetween: 0,
+
+  //slidesPerView: 1.3,
+  //spaceBetween: 32,
+  //slidesOffsetBefore: 16,
+  //    slidesOffsetAfter: 16,
+  //    centeredSlidesBounds: true,
   pagination: {
   el: ".swiper-pagination",
   clickable: true,
   },
 });
-const myDiv = document.getElementById('show-more_ikon');
-const toggleButton = document.getElementById('show_icon-readmore');
+//const myDiv = document.getElementById('show-more_ikon');
+//const toggleButton = document.getElementById('open-icon');
 
-  toggleButton.addEventListener('click', function() {
-      if (myDiv.style.display === 'none') {
-        myDiv.style.display = 'block';
-        toggleButton.textContent = 'Скрыть';
-      } else {
-        myDiv.style.display = 'none';
-        toggleButton.textContent = 'Показать';
-      }
-    });
+//  toggleButton.addEventListener('click', function() {
+//      if (myDiv.style.display === 'none') {
+//        myDiv.style.display = 'block';
+//        toggleButton.textContent = 'Скрыть';
+//      } else {
+//        myDiv.style.display = 'none';
+//        toggleButton.textContent = 'Показать';
+//      }
+//    });
+
+const iconbutton = document.getElementById("iconbutton");
+const toggleText = iconbutton.querySelector(".open_icon_text");
+const mainImg = document.querySelector(".main-img");
+const gridItems = document.querySelectorAll(".swiper-slide");
+btn.addEventListener("click", btnClick);
+
+function btnClick() {
+    console.log(content.classList);
+
+    if (content.classList.contains("hidden")) {
+        btn.textContent = "Скрыть элемент";
+    } else {
+        btn.textContent = "Показать элемент";
+    }
+
+    content.classList.toggle("hidden");
+}
