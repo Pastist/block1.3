0dbacecef095096e21af441c43b283392d1b3420
Sergey Prohorenko <93403270+Pastist@users.noreply.github.com>
2025-09-08 16:46:45 +0300

    1.5.320

--- js/main.js
+++ js/main.js
@@ -13,8 +13,6 @@ const swiper = new Swiper('.swiper', {
   },
 });
 
-
-
 const toggleButton = document.getElementById("iconButton");
 const toggleText = toggleButton.querySelector(".open_icon_text");
 const mainImg = document.querySelector(".open_img");
@@ -38,3 +36,33 @@ toggleButton.addEventListener("click", () => {
   }
   isExpanded = !isExpanded;
 });
+
+//const toggleButton = document.getElementById("iconButton");
+//const toggleText = toggleButton.querySelector(".open_icon_text");
+//const mainImg = document.querySelector(".open_img");
+//const closeImg = document.querySelector(".element_stail");
+//const slideToggle = document.querySelectorAll(".brands_services");
+//let isExpanded = false;
+//toggleButton.addEventListener("click", () => {
+//  if (isExpanded) {
+//    slideToggle.forEach((item, index) => {
+//      if (window.innerWidth >= 1010) {
+//        if (index >= 8) item.classList.add("hidden");
+//      } else if (window.innerWidth >= 768) {
+//        if (index >= 6) item.classList.add("hidden");
+//      }
+//    });
+//    toggleText.textContent = "Показать всё";
+//    mainImg.classList.remove("element_stail");
+//    mainImg.classList.add("open_img");
+    
+//  } else {
+//    slideToggle.forEach(item => item.classList.remove("hidden"));
+//    toggleText.textContent = "Скрыть";
+//    mainImg.classList.remove("nul");
+//    mainImg.classList.add("element_stail");
+
+
+//  }
+//  isExpanded = !isExpanded;
+//});
