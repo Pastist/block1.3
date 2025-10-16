322ddc8af511aae440384fb8de4d27d344d821de
Sergey Prohorenko <93403270+Pastist@users.noreply.github.com>
2025-10-17 00:17:34 +0300

    final

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,165 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const { extendDefaultPlugins } = require("svgo");
+
+const json = require('./src/js/main.js');
+const project_name = json.name;
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const filename = (ext) => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
+const optimization = () => {
+  const configObj = {
+    splitChunks: {
+      chunks: 'all',
+    },
+  }
+
+  if (isProd) {
+    configObj.minimizer = [
+      new CssMinimizerWebpackPlugin(),
+      new TerserWebpackPlugin(),
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            // Lossless optimization with custom option
+            // Feel free to experiment with options for better result for you
+            plugins: [
+              ["gifsicle", { interlaced: true }],
+              ["jpegtran", { progressive: true }],
+              ["optipng", { optimizationLevel: 5 }],
+              // Svgo configuration here https://github.com/svg/svgo#configuration
+              [
+                "svgo",
+                {
+                  plugins: extendDefaultPlugins([
+                    {
+                      name: "removeViewBox",
+                      active: false,
+                    },
+                    {
+                      name: "addAttributesToSVGElement",
+                      params: {
+                        attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
+                      },
+                    },
+                  ]),
+                },
+              ],
+            ],
+          },
+        },
+      }),
+    ];
+  }
+  return configObj;
+}
+
+const plugins = () => {
+  const basePlugins = [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      filename: 'index.html',
+      minify: {
+        collapseWhitespace: isProd
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: `./css/${filename('css')}`,
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/assets'), to: path.resolve(__dirname, 'app/assets'),
+        }
+      ]
+    }),
+  ];
+
+  return basePlugins;
+}
 
 module.exports = {
+  mode: 'development',
+  context: path.resolve(__dirname, 'src'),
   entry: './src/js/main.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.[contenthash].js',
+    filename: `./js/${filename('js')}`,
+    path: path.resolve(__dirname, 'app'),
     clean: true,
-    assetModuleFilename: 'assets/[name][ext]',
+    publicPath: '',
   },
+  devServer: {
+    historyApiFallback: true,
+    open: true,
+    compress: true,
+    hot: true,
+    port: 3000,
+  },
+  optimization: optimization(),
+  plugins: plugins(),
+  devtool: isProd ? false : 'source-map',
   module: {
     rules: [
-      // JS / Babel - УПРОЩАЕМ КОНФИГУРАЦИЮ
       {
-        test: /\.js$/,
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev
+            },
+          },
+          "css-loader"
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [{
+          loader: MiniCssExtractPlugin.loader,
+          options: {
+            publicPath: (resourcePath, context) => {
+              return path.relative(path.dirname(resourcePath), context) + '/';
+            }
+          }
+        },
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.js$/i,
         exclude: /node_modules/,
-        use: 'babel-loader' // ← УБИРАЕМ options отсюда
-      },
-
-      // SASS / CSS - ВЫБИРАЕМ ОДИН ПОДХОД:
-
-      // Вариант для разработки (с style-loader):
-      {
-        test: /\.(scss|css)$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-
-      // ИЛИ Вариант для production (с MiniCssExtractPlugin):
-      // {
-      //   test: /\.(scss|css)$/,
-      //   use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-      // },
-
-      // Остальные правила остаются без изменений...
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'asset/resource',
-        //generator: {
-        //  filename: 'assets/icon/[name][ext]',
-        //},
+        use: ['babel-loader'],
       },
       {
-      test: /\.html$/i, // применяется к файлам .html
-      loader: "html-loader",
-      },
-      {
-        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i,
+        test: /\.(?:|jpe?g|png|gif|svg|ico)$/i,
         type: 'asset/resource',
         generator: {
-          filename: 'assets/fonts/[name][ext]',
+          filename: () => {
+            return isDev ? 'img/[name][ext]' : 'img/[name].[contenthash][ext]';
+          },
         },
       },
-    ],
+      {
+        test: /\.(?:|woff2|woff)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: () => {
+            return isDev ? 'fonts/[name][ext]' : 'fonts/[name].[contenthash][ext]';
+          },
+        },
+      },
+    ]
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-    // Раскомментируйте если используете MiniCssExtractPlugin.loader:
-     new MiniCssExtractPlugin({
-       filename: '[name].[contenthash].css',
-     }),
-  ],
-};
+}
